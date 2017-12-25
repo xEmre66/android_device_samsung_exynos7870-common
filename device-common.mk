6017982acbc0c14e480f778b8041476228f8d046
DarkLord1731 <darklord1731@gmail.com>
2017-12-25 15:19:36 +0530

    Add gralloc package

--- device-common.mk
+++ device-common.mk
@@ -68,6 +68,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 PRODUCT_PACKAGES += \
+gralloc.exynos5
+
+PRODUCT_PACKAGES += \
     libion \
     libfimg
 
