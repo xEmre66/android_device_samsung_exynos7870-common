53add181fd0356fea43f9791c4afffba0d430a21
DarkLord1731 <darklord1731@gmail.com>
2018-01-29 14:09:41 +0530

    exynos7870-common: Move seccomp files in configs/

--- product/seccomp.mk
+++ product/seccomp.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-# seccomp
+# Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
